5dc6b1a9dc1e264b2038431bad50ddd6427ba64e
HyunTaek Oh <oht222@naver.com>
2024-08-21 03:14:42 +0900

    chore: swagger 관련 세팅 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Wish Tree API')
+    .setDescription('Wish Tree API 문서입니다.')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.API_PORT || 8000);
 }
+
 bootstrap();
